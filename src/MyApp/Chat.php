4efd26ea668c57e9f72b9ffd19b2a00158a0bc98
Jonathan Vuillemin <jonathan.vuillemin@doclerholding.com>
2017-02-16 14:53:38 +0100

    initial commit

---
+++ src/MyApp/Chat.php
@@ -0,0 +1,88 @@
+<?php
+namespace MyApp;
+
+use Exception;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use SplObjectStorage;
+
+/**
+ * Class Chat
+ *
+ * @package MyApp
+ */
+class Chat implements MessageComponentInterface
+{
+
+	/**
+	 * @var SplObjectStorage
+	 */
+	protected $clients;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		$this->clients = new SplObjectStorage;
+	}
+
+	/**
+	 * @param ConnectionInterface $conn
+	 */
+	public function onOpen(ConnectionInterface $conn)
+	{
+		// Store the new connection to send messages to later
+		$this->clients->attach($conn);
+
+		echo "New connection! ({$conn->resourceId})\n";
+	}
+
+	/**
+	 * @param ConnectionInterface $from
+	 * @param string              $msg
+	 */
+	public function onMessage(ConnectionInterface $from, $msg)
+	{
+		$numRecv = count($this->clients) - 1;
+
+		echo sprintf(
+			'Connection %d sending message "%s" to %d other connection %s' . "\n",
+			$from->resourceId,
+			$msg,
+			$numRecv,
+			$numRecv == 1 ? '' : 's'
+		);
+
+		foreach ($this->clients as $client)
+		{
+			if ($from !== $client)
+			{
+				// The sender is not the receiver, send to each client connected
+				$client->send($msg);
+			}
+		}
+	}
+
+	/**
+	 * @param ConnectionInterface $conn
+	 */
+	public function onClose(ConnectionInterface $conn)
+	{
+		// The connection is closed, remove it, as we can no longer send it messages
+		$this->clients->detach($conn);
+
+		echo "Connection {$conn->resourceId} has disconnected\n";
+	}
+
+	/**
+	 * @param ConnectionInterface $conn
+	 * @param Exception           $e
+	 */
+	public function onError(ConnectionInterface $conn, Exception $e)
+	{
+		echo "An error has occurred: {$e->getMessage()}\n";
+
+		$conn->close();
+	}
+}
